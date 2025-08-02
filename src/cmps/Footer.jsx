612ee7313e7ea43f7c3dbab6623fffa676cc5da7
adirahav <adi_rahav@yahoo.com>
2025-08-02 18:50:00 +0300

    fix web url

--- src/cmps/Footer.jsx
+++ src/cmps/Footer.jsx
@@ -47,7 +47,7 @@ export function Footer() {
                         <li><NavLink to="/terms-of-use"><TermsOfUseIcon sx={IconSizes.Small} /><span>{utilService.getPhrase("drawer_terms_of_use", phrases)}</span></NavLink></li>
                         {showAllFooter && <li><NavLink to="/contact-us"><ContactUsIcon sx={IconSizes.Small} /><span>{utilService.getPhrase("drawer_contact_us", phrases)}</span></NavLink></li>}
                         {showAllFooter && <li><NavLink to={share?.url} rel="nofollow noopener" target="_blank"><ShareIcon sx={IconSizes.Small} /><span>{share?.text}</span></NavLink></li>}
-                        {showAllFooter && <li><NavLink onClick={openWebBrowser} to={url} rel="nofollow noopener" target="_blank">{share?.moreIcon === "web" ? <WebIcon sx={IconSizes.Small} /> : <AndroidIcon sx={IconSizes.Small} />}<span>{share?.moreText}</span></NavLink></li>}
+                        {showAllFooter && <li><NavLink onClick={openWebBrowser} to={share?.moreUrl} rel="nofollow noopener" target="_blank">{share?.moreIcon === "web" ? <WebIcon sx={IconSizes.Small} /> : <AndroidIcon sx={IconSizes.Small} />}<span>{share?.moreText}</span></NavLink></li>}
                         {showAllFooter && <li><span>|</span></li>}
                         {showAllFooter && <li><span>{utilService.getPhrase("drawer_version", phrases).replace("%1$s", parseFloat(share?.versionNumber).toFixed(1))}</span></li>}
                     </ul>
